14114672fc5e7537e3c937e86bf31977e113baa8
Rares Luncan <luncanraresh@gmail.com>
2017-08-01 17:35:19 +0300

    Resolved conflicts and indentation problems

--- firmware.py
+++ firmware.py
@@ -6,9 +6,12 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app=Flask(__name__)
 app.config.from_object(__name__)
 
+
 @app.route('/')
 def show_home():
    return render_template('index.html')
+
+   
 @app.route('/details')
 def show_details():
    return render_template('details.html')
