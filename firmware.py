c040616d4a8d01cbd41b8e9a4a8023d0a5286ccd
Rares Luncan <luncanraresh@gmail.com>
2017-08-02 17:31:20 +0300

    Added database

--- firmware.py
+++ firmware.py
@@ -5,13 +5,55 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 app=Flask(__name__)
 app.config.from_object(__name__)
+app.config.update(dict(
+    DATABASE = os.path.join(app.root_path, 'firmware.db'),
+    SECRET_KEY='such secret much wow so key no guess muh'
+))
+
+
+def connect_db():
+    rv=sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+    
+def init_db():
+    db=get_db()
+    with app.open_resource('schema.sql',mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    init_db()
+    print("Firmware initialized database sucessfully!")
+def get_db():
+    if not hasattr(g,'sqlite_db'):
+        g.sqlite_db=connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 
 @app.route('/')
 def show_home():
-   return render_template('index.html')
+    db = get_db()
+    cur = db.execute("select * from companies")
+    companies = cur.fetchall()
+    return render_template('index.html',companies=companies)
 
-   
-@app.route('/details')
-def show_details():
-   return render_template('details.html')
+
+@app.route('/details/<company_id>')
+def show_details(company_id):
+    db = get_db()
+    cur = db.execute("select user, review from reviews where company_id="+company_id+" order by id desc")
+    reviews = cur.fetchall()
+    db = get_db()
+    cur = db.execute("select * from companies where company_id="+company_id)
+    rows=cur.fetchall()
+    return render_template('details.html', reviews=reviews, rows=rows)
---
+++ schema.sql
@@ -0,0 +1,15 @@
+create table if not exists reviews (
+  	`id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+  	`user`	TEXT NOT NULL,
+  	`review`	TEXT NOT NULL,
+  	`company_id`	INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS companies (
+	`company_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
+	`company_name`	TEXT NOT NULL UNIQUE,
+  `company_description` TEXT NOT NULL,
+	`company_details`	TEXT NOT NULL,
+  `company_rating` TEXT NOT NULL,
+  `company_logo` TEXT NOT NULL
+
+);
