171f25df94d7438afb519441cd048123c2e4b8e7
Rares Luncan <luncanraresh@gmail.com>
2017-08-03 13:56:01 +0300

    Some issues fixed

--- firmware.py
+++ firmware.py
@@ -7,7 +7,7 @@ app=Flask(__name__)
 app.config.from_object(__name__)
 app.config.update(dict(
     DATABASE = os.path.join(app.root_path, 'firmware.db'),
-    SECRET_KEY='such secret much wow so key no guess muh'
+    SECRET_KEY = 'such secret much wow so key no guess muh'
 ))
 
 
@@ -16,7 +16,7 @@ def connect_db():
     rv.row_factory = sqlite3.Row
     return rv
 
-    
+
 def init_db():
     db=get_db()
     with app.open_resource('schema.sql',mode='r') as f:
@@ -28,9 +28,11 @@ def init_db():
 def initdb_command():
     init_db()
     print("Firmware initialized database sucessfully!")
+
+
 def get_db():
-    if not hasattr(g,'sqlite_db'):
-        g.sqlite_db=connect_db()
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
     return g.sqlite_db
 
 
@@ -40,20 +42,19 @@ def close_db(error):
         g.sqlite_db.close()
 
 
-@app.route('/')
-def show_home():
+@app.route('/home')
+def home():
     db = get_db()
     cur = db.execute("select * from companies")
     companies = cur.fetchall()
-    return render_template('index.html',companies=companies)
+    return render_template('index.html', companies=companies)
 
 
 @app.route('/details/<company_id>')
-def show_details(company_id):
+def details(company_id):
     db = get_db()
-    cur = db.execute("select user, review from reviews where company_id="+company_id+" order by id desc")
-    reviews = cur.fetchall()
-    db = get_db()
-    cur = db.execute("select * from companies where company_id="+company_id)
-    rows=cur.fetchall()
-    return render_template('details.html', reviews=reviews, rows=rows)
+    reviewsCursor = db.execute('select user, review from reviews where company_id="%s" order by id desc' % company_id)
+    reviews = reviewsCursor.fetchall()
+    companyCursor = db.execute('select * from companies where company_id="%s"' % company_id)
+    company = companyCursor.fetchall()
+    return render_template('details.html', reviews=reviews, company=company)
