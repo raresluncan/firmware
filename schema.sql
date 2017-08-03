deefedd91c462dd8a0c637d0900feac596a3ceba
Rares Luncan <luncanraresh@gmail.com>
2017-08-03 15:55:22 +0300

    removed unnecessary lines

--- schema.sql
+++ schema.sql
@@ -1,30 +1,14 @@
 create table if not exists reviews (
-<<<<<<< HEAD
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `user` TEXT NOT NULL,
   `review` TEXT NOT NULL,
   `company_id` INTEGER NOT NULL
-=======
-  `id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-  `user`	TEXT NOT NULL,
-  `review`	TEXT NOT NULL,
-  `company_id`	INTEGER NOT NULL
->>>>>>> Changes requested
 );
 
 
 CREATE TABLE IF NOT EXISTS companies (
-<<<<<<< HEAD
   `company_id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `company_name` TEXT NOT NULL UNIQUE,
   `company_description` TEXT NOT NULL,
   `company_details` TEXT NOT NULL,
-=======
-  `company_id`	INTEGER PRIMARY KEY AUTOINCREMENT,
-  `company_name`	TEXT NOT NULL UNIQUE,
-  `company_description` TEXT NOT NULL,
-  `company_details`	TEXT NOT NULL,
->>>>>>> Changes requested
-  `company_rating` TEXT NOT NULL,
-  `company_logo` TEXT NOT NULL
 );
