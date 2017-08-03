6e3541525bd9c6306baabd9f253889fe6cffa7a7
Rares Luncan <luncanraresh@gmail.com>
2017-08-03 15:58:13 +0300

    database bug fixed

--- schema.sql
+++ schema.sql
@@ -11,4 +11,6 @@ CREATE TABLE IF NOT EXISTS companies (
   `company_name` TEXT NOT NULL UNIQUE,
   `company_description` TEXT NOT NULL,
   `company_details` TEXT NOT NULL,
+  `company_rating` TEXT NOT NULL,
+  `company_logo` TEXT NOT NULL
 );
